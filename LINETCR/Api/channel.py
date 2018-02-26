b0642b7ff3f0f2fa9618b5da523b5bc602d73988
ariyans379 <33821665+ariyans379@users.noreply.github.com>
2018-02-26 19:08:08 +0700

    channel.py

--- LINETCR/Api/channel.py
+++ LINETCR/Api/channel.py
@@ -19,7 +19,7 @@ class Channel:
     channel_query_path = "/CH4"
 
     UA = "Line/1.4.17"
-    LA = "IOSIPAD\x097.14.0\x09iPhone_OS\x0910.12.0"
+    LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
     
 
     authToken = None
